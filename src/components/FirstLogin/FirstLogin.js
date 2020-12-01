3bf4864b2a95961cf202657d0547bfbcc144e035
jiwon <jidole041214@naver.com>
2020-12-01 23:49:50 +0900

    서버연동

--- src/components/FirstLogin/FirstLogin.js
+++ src/components/FirstLogin/FirstLogin.js
@@ -4,10 +4,13 @@ import * as F from './FirstLoginStyle'
 import img from './첫번째배경.png'
 import styled from 'styled-components';
 import './border.css'
+import axios from 'axios'
 
 const First=()=>{
     let i = 0;
     const [wid, Cwid] = useState(0)
+    const [blind, see] = useState(false)
+    const [inputVal, change] = useState('')
     function Deacrease(){
         console.log('sdf');
             let rep = setInterval(()=>{
@@ -20,6 +23,21 @@ const First=()=>{
                 console.log(i)
             },5)
     }
+
+    function postServer() {
+        axios({
+            method: 'post',
+            url: 'http://easyair.herokuapp.com/team',
+            headers:{
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + window.localStorage.getItem("token")
+            },
+            data:{
+                "name" : inputVal
+            }
+        })
+    }
+
    useEffect(()=>{ // 컴포넌트 첫 실행시 작동
         setInterval(()=>{
             Cwid(i);
@@ -31,6 +49,16 @@ const First=()=>{
                     
         },15)
     },[])
+    function buttonAppear(e){
+        if(e.target.value != ""){
+            see(true)
+        }
+        else{
+            see(false)
+        }
+        change(e.target.value)
+        console.log(inputVal)
+    }
     const style={
         width: wid+'px',height:'45px', overflow:'hidden',marginBottom:'50px',
         marginRight:'300px'
@@ -43,7 +71,8 @@ const First=()=>{
                 <h3 className="blink"> 팀 이름을 <i>등록</i>해 주세요!</h3>
                 </div>
                 </article>
-                <input type="text" placeholder="팀 이름을 입력해주세요!"/>
+                <input type="text" placeholder="팀 이름을 입력해주세요!" onChange={buttonAppear}/>
+               <Link to="/company">{blind? <button onClick={postServer}>등록하기</button> : ''}</Link>
             </F.InputContainer>
             <img src={img}></img>
         </div>
